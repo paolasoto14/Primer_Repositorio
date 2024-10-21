e0b0e01885058db0f73383c0d76600154ef4d0f0
Paola <dannapaolasotoflores523@gmail.com>
2024-10-21 09:31:05 -0600

    Add files via upload

---
+++ P00Presentación.php
@@ -0,0 +1,13 @@
+<?php
+/*CBTIS 89
+P00Presentación 
+Programa contiene mis datos personales
+Danna Paola Soto Flores
+3°A Programación Matutino */
+
+echo "Mi nombre es Danna Paola Soto Flores";
+echo "<br>","Estudio en el CBTIS 89";
+echo  "<br>","En 3er semestre de la carrera de Programación A Matutino";
+echo "<br>", "Vivo en la Ciudad de Durango, Dgo.";
+
+?>
