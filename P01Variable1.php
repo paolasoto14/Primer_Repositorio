5d947753eeea943578ef3698280b1313119e4c27
Paola <dannapaolasotoflores523@gmail.com>
2024-10-21 09:10:09 -0600

    Add files via upload

---
+++ P01Variable1.php
@@ -0,0 +1,25 @@
+<?php
+/* CBTIS 89
+   Programa que une cadenas de texto
+   Danna Paola Soto Flores
+   3°A Programacion Matutino
+*/ 
+
+$dia = 24;
+$sueldo = 758.43;
+$nombre = "Juan";
+$exite = true;
+
+echo "Variable entera:";
+echo $dia;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre;echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. "    ".$cadena2;
+?>
